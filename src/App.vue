afa7cbefcaac782143f36d2da1cdb985a31ce959
Rishav Rastogi <rishavrastogi@Rishavs-iMac.local>
2022-10-07 23:48:08 -0700

    First index

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
+  <header>    
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h2>Hello World!</h2>
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main>    
   </main>
 </template>
 
---
+++ src/mainIntro.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import AppIntro from "./AppIntro.vue";
+
+import "./assets/main.css";
+
+createApp(AppIntro).mount("#app");
